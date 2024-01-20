0397673829c547ca072ccb6e012c90aae3348edf
Pujon Das Auvi <auvipujondas@gmail.com>
2024-01-21 00:17:00 +0600

    payment schema updated

--- src/model/payment.schema.js
+++ src/model/payment.schema.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const paymentSchema = mongoose.Schema({
-    userId:{},
-    orderId: {type : String, required: true },  // reference to the Order model
-    sessionId: { type: String, required: true },
-    paid: { type: Boolean , default: false },
-    date: {
-        type: String,
-        default: Date.now
-      },
-      email:{}
-})
+  userId: { type: String, require: true },
+  orderId: { type: String, required: true }, // reference to the Order model
+  sessionId: { type: String, required: true },
+  paid: { type: String, enum: ["paid", "unpaid"], default: "unpaid" },
+  date: {
+    type: String,
+    default: Date.now,
+  },
+  email: { type: String, require: true },
+});
