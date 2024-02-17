15649aa36163d160efb68e023c3c22b6a5f7fa76
Pujon Das Auvi <auvipujondas@gmail.com>
2024-02-17 18:54:30 +0600

    authorization added in orders route

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const {getAllOrders, createOrder, getOrderByEmail, updateOrderById, deleteOrderById, deleteOrdersByEmail} = require('../controllers/order.controller');
+const {verifyJWT, verifyAdmin}=require('../middlewares/auth');
 
 
 // Route to get all orders
-router.get('/', getAllOrders);
+router.get('/',verifyJWT, getAllOrders);
 
 // Route to create a new order
-router.post('/', createOrder);
+router.post('/',verifyJWT, createOrder);
 
 
 // Route to update an order by ID
-router.put('/:id', updateOrderById);
+router.put('/:id',verifyJWT, updateOrderById);
 
 // Route to delete an order by ID
-router.delete('/:id', deleteOrderById);
+router.delete('/:id',verifyJWT, verifyAdmin, deleteOrderById);
 
 // Route to get orders by user email
-router.get('/:email', getOrderByEmail);
+router.get('/:email',verifyJWT, getOrderByEmail);
 
 // Route to delete all orders with the same email
-router.delete('/email/:email', deleteOrdersByEmail);
+router.delete('/email/:email',verifyJWT,verifyAdmin, deleteOrdersByEmail);
 
 module.exports = router;
