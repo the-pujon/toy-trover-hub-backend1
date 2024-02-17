c16e06aa3b6b5b5c0acdd807d49d15fd0b7d1ab6
Pujon Das Auvi <auvipujondas@gmail.com>
2024-02-17 18:51:37 +0600

    authorization added in payments route

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,25 +1,26 @@
 const router = require("express").Router();
-const {createPayment, getAllPayments, getPaymentByEmail, getPaymentById, deletePaymentById, updatePaymentByOrderId} = require('../controllers/payment.controller')
+const {createPayment, getAllPayments, getPaymentByEmail, getPaymentById, deletePaymentById, updatePaymentByOrderId} = require('../controllers/payment.controller');
+const {verifyAdmin, verifyJWT}=require("../middlewares/auth");
 
 
 // Create a new payment
-router.post('/', createPayment);
+router.post('/',verifyJWT, createPayment);
 
 // Get all payments
-router.get('/', getAllPayments);
+router.get('/',verifyJWT, verifyAdmin, getAllPayments);
 
 // Get payment by email
-router.get('/email/:email', getPaymentByEmail);
+router.get('/email/:email',verifyJWT, getPaymentByEmail);
 
 // Get payment by ID
-router.get('/id/:id', getPaymentById);
+router.get('/id/:id',verifyJWT, getPaymentById);
 
 //TODO: write update payment by order ID test case
 // update payment by order ID
-router.put('/orderId/:orderId', updatePaymentByOrderId);
+router.put('/orderId/:orderId',verifyJWT, updatePaymentByOrderId);
 
 // Delete payment by ID
-router.delete('/id/:id', deletePaymentById);
+router.delete('/id/:id',verifyJWT, deletePaymentById);
 
 // Export the router
 module.exports = router;
