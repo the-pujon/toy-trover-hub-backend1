8ef6d1f2874e422bf2b2e63b4095cae9f047eae2
Pujon Das Auvi <auvipujondas@gmail.com>
2024-02-22 13:21:26 +0600

    origin updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const corsOptions = {
-  origin: `${process.env.FRONTEND_URL}`,
+  origin: `${process.env.FRONTEND_URL  || 'http://localhost:5173/'}`,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
